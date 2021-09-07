e8d2c6d9b483ac98878d06b905d93cdf699c3ee3
anuradha9712 <anuradha.mcs18.du@gmail.com>
2021-09-07 19:49:47 +0530

    fix(Mention): add link as a Mention

---
+++ stories/MentionCustomRenderer/index.story.js
@@ -0,0 +1,53 @@
+/* @flow */
+
+import React from 'react';
+import { Editor } from '../../src';
+import { Text } from '@innovaccer/design-system';
+import { disabledArgtypes, commonArgs } from '../__common__/argTypes';
+
+export const All = (args) => {
+  const customRenderer = (props) => (
+    <Text appearance="link">
+      {props}
+    </Text>
+  );
+
+  return (
+    <Editor
+      {...args}
+      mention={{
+        label: ' ',
+        trigger: '@',
+        renderer: customRenderer,
+        suggestions: [
+          { label: 'First Name', value: 'First Name' },
+          { label: 'Last Name', value: 'Last Name' },
+          { label: 'PCP', value: 'PCP' },
+          { label: 'Address', value: 'Address' },
+          { label: 'DOB', value: 'DOB' },
+        ],
+      }}
+    />
+  );
+};
+
+All.argTypes = {
+  ...disabledArgtypes,
+  mention: { control: { disable: true } }
+};
+
+All.args = {
+  ...commonArgs
+};
+
+export default {
+  title: 'Library/Mention - CustomRenderer',
+  component: Editor,
+  parameters: {
+    docs: {
+      source: {
+        type: 'code',
+      }
+    },
+  },
+};
