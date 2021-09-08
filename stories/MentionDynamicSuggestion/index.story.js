400494ba16cfec9467980bd30300630c4e717139
anuradha9712 <anuradha.mcs18.du@gmail.com>
2021-09-08 18:51:19 +0530

    feat(Mention): adds dynamic suggestion option

---
+++ stories/MentionDynamicSuggestion/index.story.js
@@ -0,0 +1,59 @@
+/* @flow */
+
+import React from 'react';
+import { Editor } from '../../src';
+import { disabledArgtypes, commonArgs } from '../__common__/argTypes';
+import { searchElement } from '../../src/utils/common';
+
+export const All = (args) => {
+
+  let suggestionList = [
+    { label: 'Gender', value: 'Gender' },
+    { label: 'ID', value: 'ID' },
+    { label: 'Address', value: 'Address' },
+    { label: 'DOB', value: 'DOB' },
+    { label: 'Age', value: 'Age' },
+    { label: 'First Name', value: 'First Name' },
+  ];
+
+  const fetchSuggestions = (searchTerm) => {
+    const searchedOptions = searchTerm ? searchElement(suggestionList, searchTerm, false) : suggestionList;
+    const result = new Promise(resolve => {
+      window.setTimeout(() => {
+        resolve(searchedOptions);
+      }, 1000);
+    });
+    return result;
+  };
+
+  return (
+    <Editor
+      {...args}
+      mention={{
+        label: ' ',
+        trigger: '@',
+        fetchSuggestions: fetchSuggestions,
+      }}
+    />
+  );
+};
+
+All.argTypes = {
+  ...disabledArgtypes,
+};
+
+All.args = {
+  ...commonArgs
+};
+
+export default {
+  title: 'Library/Mention - DynamicSuggestion',
+  component: Editor,
+  parameters: {
+    docs: {
+      source: {
+        type: 'code',
+      }
+    },
+  },
+};
