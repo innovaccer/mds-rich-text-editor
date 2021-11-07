3da3a68d3db8735fd3f3952a4acab8eb4dc8ca2b
Anuradha aggarwal <anuradha.mcs18.du@gmail.com>
2021-11-07 22:07:50 +0530

    feat(Image): add image alignment option

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,5 +13,15 @@ export const htmlToState = (html) => {
 
 export const stateToHTML = (editorState) => {
   const json = convertToRaw(editorState.getCurrentContent());
-  return draftToHtml(json);
+
+  return draftToHtml(json, {}, false, ({ type, data }) => {
+    if (type === 'IMAGE') {
+      const alignment = data.alignment ? data.alignment : 'center';
+      return `
+        <p style="justify-content:${alignment}; display:flex">
+          <img src="${data.src}" alt="${data.alt}" style="height: ${data.height};width: ${data.width}"/>
+        </p>
+      `;
+    }
+  });
 };
