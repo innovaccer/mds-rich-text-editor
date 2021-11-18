2117698dd521086a768cc3d0604c04fcc1eeaaa5
Satyam Yadav <satyamyadav3@gmail.com>
2021-11-18 20:22:37 +0530

    chore: adds actiions to release

---
+++ scripts/version.sh
@@ -0,0 +1,13 @@
+LOG=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%h %s" --no-merges | sed -e 's/^/* /')
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+DATE=$(date +%F)
+
+# Append changelog
+echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG";
+
+# build
+npm run build && 
+
+# add changes to git
+git add dist -f &&
+git add public -f;
