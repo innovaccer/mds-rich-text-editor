fb697192812f8bc03cb9d029f2baf4d481d059d2
Anuradha aggarwal <anuradha.mcs18.du@gmail.com>
2022-01-27 16:12:11 +0530

    fix(mention): add story to get mention list

---
+++ stories/MentionList/index.story.js
@@ -0,0 +1,62 @@
+/* @flow */
+
+import React from 'react';
+import { Editor } from '../../src';
+import { Heading, Chip } from '@innovaccer/design-system';
+import { disabledArgtypes, commonArgs } from '../__common__/argTypes';
+
+export const All = (args) => {
+
+  const [users, setUsersList] = React.useState([]);
+
+  const onEditorStateChange = (editorState) => {
+    const contentState = Editor.utils.convertToRaw(editorState.getCurrentContent());
+    const entityMap = contentState.entityMap;
+    const mentionList = Object.values(entityMap).map(entity => entity.data.value);
+    setUsersList(mentionList);
+  };
+
+  return (
+    <div>
+      <Editor
+        {...args}
+        onEditorStateChange={onEditorStateChange}
+        mention={{
+          label: ' ',
+          trigger: '@',
+          suggestions: [
+            { label: 'First Name', value: 'fn' },
+            { label: 'Last Name', value: 'ln' },
+            { label: 'PCP', value: 'PCP' },
+            { label: 'Address', value: 'add' },
+            { label: 'DOB', value: 'dob' },
+          ],
+        }}
+      />
+      <br />
+      <Heading size="s">List of all Mentions:</Heading>
+      {users.map((item) => <Chip className="my-4" label={item} type="action" />)}
+    </div>
+  );
+};
+
+All.argTypes = {
+  ...disabledArgtypes,
+  mention: { control: { disable: true } },
+};
+
+All.args = {
+  ...commonArgs,
+};
+
+export default {
+  title: 'Library/Mention - MentionList',
+  component: Editor,
+  parameters: {
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+  },
+};
