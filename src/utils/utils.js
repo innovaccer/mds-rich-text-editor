f77b9b445c4578e6aa0024ed43af28374d553ee3
Varnika <varnika.jain@innovaccer.com>
2022-11-03 15:16:16 +0530

    fix(preview): content to html

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,12 @@
 import htmlToDraft from 'html-to-draftjs';
 import draftToHtml from 'draftjs-to-html';
+import redraft from 'redraft';
 import { ContentState, EditorState, convertToRaw } from 'draft-js';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 export const htmlToState = (html) => {
   // Remove extra newline in html generated from Preview component
-  const imgContent = html.replaceAll('<p><br/></p><p id="RichTextEditor-Image"','<p id="RichTextEditor-Image"');
+  const imgContent = html.replaceAll('<p><br/></p><p id="RichTextEditor-Image"', '<p id="RichTextEditor-Image"');
   const htmlContent = imgContent.replaceAll('<br/>', '');
   const contentBlock = htmlToDraft(htmlContent);
   if (contentBlock) {
@@ -14,10 +16,24 @@ export const htmlToState = (html) => {
   }
 };
 
-export const stateToHTML = (editorState) => {
+const getPreviewComponent = (raw, { entities = {} }) => {
+  const updatedEntities = { ...entities };
+  const renderer = {
+    entities: updatedEntities,
+  };
+  return redraft(raw, renderer);
+};
+
+export const stateToHTML = (editorState, renderer = {}) => {
   const json = convertToRaw(editorState.getCurrentContent());
+  const previewComponent = getPreviewComponent(json, renderer);
+  var mentionIndex = 0;
+  const mentionCheck = (i) => {
+    return previewComponent[0][0][i] !== undefined && "type" in previewComponent[0][0][i]
+  }
 
   return draftToHtml(json, {}, false, ({ type, data }) => {
+
     if (type === 'IMAGE') {
       const alignment = data.alignment ? data.alignment : 'left';
       return `
@@ -26,5 +42,27 @@ export const stateToHTML = (editorState) => {
         </p>
       `;
     }
-  });
-};
+    if (type === 'MENTION') {
+      const contentLength = previewComponent[0][0].length
+      if (mentionIndex === 0) {
+        for (var i = 1; i < contentLength; i++) {
+          if (mentionCheck(i)) {
+            mentionIndex = i;
+            break;
+          }
+        }
+        return `${renderToStaticMarkup(previewComponent[0][0][mentionIndex])}`;
+      }
+      else {
+        for (var i = mentionIndex + 1; i < contentLength; i++) {
+          if (mentionCheck(i)) {
+            mentionIndex = i;
+            break;
+          }
+        }
+        return `${renderToStaticMarkup(previewComponent[0][0][mentionIndex])}`;
+      }
+
+    }
+  })
+} 
--- stories/ConvertStateToHTML/index.story.js
+++ stories/ConvertStateToHTML/index.story.js
@@ -14,12 +14,24 @@ export const All = (args) => {
   };
 
   const onClick = () => {
-    setHTML(Editor.utils.stateToHTML(editorState));
+    const entities = {
+      MENTION: (children, entity, { key }) => <div key={key}>{entity.value}</div>,
+    };
+    const html = Editor.utils.stateToHTML(editorState, {entities});
+    setHTML(html);
   };
 
   return (
     <div>
-      <Editor {...args} editorState={editorState} onEditorStateChange={onEditorStateChange} />
+      <Editor {...args} editorState={editorState} onEditorStateChange={onEditorStateChange} mention={{
+          suggestions: [
+            { label: 'First Name', value: 'Innovaccer' },
+            { label: 'Last Name', value: 'Analytics' },
+            { label: 'PCP', value: '112' },
+            { label: 'Address', value: 'Test Address' },
+            { label: 'DOB', value: '11-02-1998' },
+          ],
+        }}/>
       <Button appearance="primary" size="large" onClick={onClick} className="mt-4 mb-5">
         Convert to HTML
       </Button>
